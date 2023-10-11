06d51cc7a767e7c7d230911be9a1115029b3ea85
Kyle Thompson <kylethompson27@outlook.com>
2023-10-10 20:54:50 -0500

    Initial Commit

---
+++ CHLDatabaseManager/CHLDatabaseManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CHLDatabaseManagerApp.swift
+//  CHLDatabaseManager
+//
+//  Created by Kyle Thompson on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CHLDatabaseManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
